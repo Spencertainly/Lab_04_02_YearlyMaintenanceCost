d10e1ed5e97fc11e4112cbf3621e0dccd61d423a
Spencer <salane2232@gmail.com>
2025-09-15 21:21:54 -0400

    Commit 2

--- src/Maintenance.java
+++ src/Maintenance.java
@@ -1,6 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class  {
-        }
+public class Maintenance {
+    public static void main(String[] args) {
+        System.out.println( "Task 2 Annual Maintenance Cost");
+        double summerCost = 64;
+        double springCost = 79;
+        double autumnCost = 46;
+        double winterCost = 35;
+        double totalCost = 0; //placeholder input for year
+
+        totalCost = springCost + summerCost + autumnCost + winterCost;
+        System.out.println( "The total annual maintenance cost is $" + totalCost);
+
     }
 }
